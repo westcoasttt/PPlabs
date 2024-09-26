3816c8f9adfc5ad4f15db519de97224c0af75b7f
westcoasttt <ilovekpop09090@gmail.com>
2024-09-26 12:56:00 +0400

    Add files via upload

---
+++ laba_№1/3num.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+ age := 26
+ height := 70.5
+ name := "Ken"
+ isAdult := true
+ fmt.Println("Возраст:", age)
+ fmt.Println("Вес:", height)
+ fmt.Println("Имя:", name)
+ fmt.Println("Взрослый:", isAdult)
+}
+
+
+
