36417a62db8ec750c51769024ae75045559096e4
westcoasttt <ilovekpop09090@gmail.com>
2024-09-26 12:55:17 +0400

    Add files via upload

---
+++ laba_№1/2nomer.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+ var age int = 26
+ var height float64 = 70.5
+ var name string = "Ken"
+ var isAdult bool = true
+ fmt.Println("Возраст:", age)
+ fmt.Println("Вес:", height)
+ fmt.Println("Имя:", name)
+ fmt.Println("Взрослый:", isAdult)
+}
